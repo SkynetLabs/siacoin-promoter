0071da8596d3f771476191dc75e9f21abc3d4aef
Chris Schinnerl <chris@skynetlabs.com>
2022-09-01 16:48:13 +0200

    fix TestHealth

--- test/database_test.go
+++ test/database_test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"testing"
+
+	"github.com/SkynetLabs/siacoin-promoter/utils"
 )
 
 // TestHealth is a simple smoke test to verify the basic functionality of the
@@ -10,8 +12,20 @@ func TestHealth(t *testing.T) {
 	if testing.Short() {
 		t.SkipNow()
 	}
+
+	// Spin up skyd instance.
+	node, err := utils.NewSkydForTesting(t.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := node.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
 	// Create tester.
-	tester, err := newTester(nil) // no skyd connection needed
+	tester, err := newTester(&node.Client)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -31,7 +45,7 @@ func TestHealth(t *testing.T) {
 	if !hg.DBAlive {
 		t.Fatal("db should be alive")
 	}
-	if hg.SkydAlive {
-		t.Fatal("skyd shouldn't be alive since we didn't pass in a client")
+	if !hg.SkydAlive {
+		t.Fatal("skyd should be alive")
 	}
 }
